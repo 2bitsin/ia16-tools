e694a710beb98008bb89ae30b80345d0b9d90699
Aleksandr Ševčenko <aleksandr.sevcenko@tesonet.com>
2023-10-21 02:15:57 +0300

    ...

--- scripts/setup-owc.sh
+++ scripts/setup-owc.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 mkdir -p /install/watcom
 pushd /install/watcom
+cp /install/scripts/options.ini /install/watcom
 wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/open-watcom-2_0-c-linux-x64
 chmod +x ./open-watcom-2_0-c-linux-x64
-xterm -e './open-watcom-2_0-c-linux-x64 -s -i -dFullInstall=true'
-echo 'export PATH=/usr/bin/watcom/binl64:$PATH' >> ~/.bashrc
-export PATH=/usr/bin/watcom/binl64:$PATH
+xterm -e './open-watcom-2_0-c-linux-x64 -f=options.ini -dFullInstall=1 -dSelectiveInstall=0 -dDstDir=/usr/bin/watcom -s -i'
+echo 'export WATCOM=/usr/bin/watcom' >> ~/.bashrc
+echo 'export PATH=$WATCOM/binl64:$PATH' >> ~/.bashrc
+export WATCOM=/usr/bin/watcom
+export PATH=$WATCOM/binl64:$PATH
 popd
 rm -rf /install/watcom
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,6 @@
 set -e 
 export DEBIAN_FRONTEND=noninteractive
 ./setup-pkg.sh
-#
 xvfb-run -a /bin/bash -c ./setup-gcc16.sh
 xvfb-run -a /bin/bash -c ./setup-owc.sh
 ./setup-ack.sh
